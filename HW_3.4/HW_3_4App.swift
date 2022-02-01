e842e98e8fb51ee1ea884241c9b1a2baa8db396e
Egor Kromin <egorcrom@mail.ru>
2022-02-01 21:19:50 +0300

    Initial Commit

---
+++ HW_3.4/HW_3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_4App.swift
+//  HW_3.4
+//
+//  Created by Егор Кромин on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
